328d04569096d5caeb7c6004c5cf2c510aff39a6
Shehryar Asif Lone <shehryar14105@gmail.com>
2026-09-17 19:46:21 +0500

    added common seed helper

---
+++ common/seed.py
@@ -0,0 +1,15 @@
+import os
+import random
+import numpy as np
+import torch
+
+#will ensure reproducibility of all results seed taken as guided
+def set_seed(seed: int = 6304) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
